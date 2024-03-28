e282ad59ff6de20fd0175a40ed9ab66ca093c68d
Aya <ayaelrhayour123@gmail.com>
2024-03-28 16:01:26 +0000

    adding relations to progress

--- app/Models/progress.php
+++ app/Models/progress.php
@@ -2,14 +2,37 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Progress extends Model
 {
     use HasFactory;
+    protected $table = "progress";
 
-    protected $hidden =[
-        'updated_at',  
+    protected $fillable = [
+        'weight',
+        'measurements',
+        'performance',
+        'height',
+        'status',
     ];
+
+    protected $hidden = [
+        'updated_at',
+    ];
+
+    public function creator(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    protected static function booted(): void{
+
+        static::addGlobalScope('user', function (Builder $builder) {
+            $builder->where('user_id', Auth::id());
+        });
+    }
 }
